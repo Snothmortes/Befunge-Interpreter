80bb5c14518c4fd54796ba786b8cef6a90fff8b8
glen <glen.jakobsen@gmail.com>
2020-12-27 12:58:40 +0100

    Add project files.

---
+++ Befunge Interpreter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Befunge_Interpreter
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            new BefungeInterpreter().Interpret(">987v>.v\nv456<  :\n>321 ^ _@");
+        }
+    }
+}
